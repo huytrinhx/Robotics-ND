199707cea9da9461899232b30aab79c6baab8d1b
Hewitt Trinh <hewitt.trinh@gmail.com>
2021-11-16 20:15:21 -0600

    Changes per feedback

--- Build-My-World/script/welcome_message.cpp
+++ Build-My-World/script/welcome_message.cpp
@@ -6,7 +6,7 @@ namespace gazebo
   {
     public: WorldPluginMyRobot() : WorldPlugin()
             {
-              printf("Welcome to My World!\n");
+              printf("Welcome to Hewitt's World!\n");
             }
 
     public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
